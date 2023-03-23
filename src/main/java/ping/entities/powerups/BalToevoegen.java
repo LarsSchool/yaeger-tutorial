81ce32e2206ee92ff5607e96cdc66577e61fcfbe
MattieJonguh <TC.Wouters1@student.han.nl>
2023-03-23 09:46:31 +0100

    Powerups bezig

--- src/main/java/ping/entities/powerups/BalToevoegen.java
+++ src/main/java/ping/entities/powerups/BalToevoegen.java
@@ -1,4 +1,18 @@
 package ping.entities.powerups;
 
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.Collider;
+
+import java.util.Random;
+
 public class BalToevoegen {
+    public int powerUpRandom;
+//    public PowerUps(Coordinate2D initialLocation) {
+//        super(initialLocation);
+//    }
+
+
+    public void doPowerUp(){
+
+    }
 }
--- src/main/java/ping/entities/powerups/Inverter.java
+++ src/main/java/ping/entities/powerups/Inverter.java
@@ -1,4 +1,17 @@
 package ping.entities.powerups;
 
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.Collider;
+
+import java.util.Random;
+
 public class Inverter {
+    public int powerUpRandom;
+//    public PowerUps(Coordinate2D initialLocation) {
+//        super(initialLocation);
+//    }
+
+
+    public void doPowerUp(){
+    }
 }
